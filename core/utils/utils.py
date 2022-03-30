7004c84ef8de0a03e131f596e16c6e426e636246
Gaston Wolfart <gastonwolfart@gmail.com>
2022-03-30 19:33:16 +0200

    added documentation

--- core/utils/utils.py
+++ core/utils/utils.py
@@ -10,6 +10,9 @@ def queue_to_list(q):
 
 
 def parse_IPs(raw):
+    """
+    Function to parse an IP list seperated by commas
+    """
     raw = "".join(raw.split())
     ips = raw.split(',')
 
@@ -17,22 +20,38 @@ def parse_IPs(raw):
 
 
 class Queue:
+    """
+    Simple FIFO queue class based on the python list
+    """
     def __init__(self, max_length=0):
         self.queue = []
         self.max_length = max_length
 
     def full(self):
+        """
+        Returns true if the list is full
+        """
         return self.max_length == len(self.queue)
 
     def put(self, item):
+        """
+        Puts an item at the end of the list and removes the first one
+        if the queue is full
+        """
         self.queue.append(item)
 
         if len(self.queue) > self.max_length and len(self.queue) > 0:
             self.queue.pop(0)
 
     def last(self):
+        """
+        Returns the last element from the queue
+        """
         if len(self.queue) > 0:
             return self.queue[-1]
 
     def dump(self):
+        """
+        Returns the current queue
+        """
         return self.queue
